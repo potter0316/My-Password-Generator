c438ec7a5d7120e096c4ea379e2ef0fa6fbcd47b
potter0316 <117317671+potter0316@users.noreply.github.com>
2022-12-21 20:15:32 -0600

    starter function

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  var length = window.prompt("choose password length between 8 and 128")
+  char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ"
+  num = "1234567890"
+  character = "!@#$%^&*()";
+  passwordText = [""]
+   for(i=0;i < length; i++){
+     passwordText += char.charAt(Math.floor(Math.random() * char.length))
+     
+  }
+  return passwordText;
+  };
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
